70b29a354dd93285456d0eefeb2dd5d8454c64d4
Mehmethan AYTEKELİ <mehmethanaytekeli@gmail.com>
2022-12-17 13:53:49 +0300

    3 Sayı Çarpma
    3 Sayı Çarpma Programı

---
+++ sayı_carpma.py
@@ -0,0 +1,6 @@
+print("3 Sayı Çarpma Programı")
+birincisayı = int(input("1.sayı:"))
+ikincisayı = int(input("2.sayı:"))
+uçuncusayı  =int(input("3.sayı:"))
+
+print("bu üç sayının çarpımı:{}".format(birincisayı * ikincisayı * uçuncusayı))
